06ec67960b393779bb215147572e97ba2ba747ce
Phil Stubbs <philipstubbs13@gmail.com>
2024-07-11 22:12:40 -0500

    intermediate nextjs

--- components/SigninForm.tsx
+++ components/SigninForm.tsx
@@ -1,10 +1,17 @@
 'use client'
 import { Input } from '@nextui-org/react'
 import Link from 'next/link'
+import SubmitButton from './SubmitButton'
+import { useFormState } from 'react-dom'
+import { signinUser } from '@/actions/auth'
+
+const initState = { message: null }
 
 const SigninForm = () => {
+  const [formState, action] = useFormState<{ message: string | null}>(signinUser, initState);
+
   return (
-    <form className="bg-content1 border border-default-100 shadow-lg rounded-md p-3 flex flex-col gap-2 ">
+    <form action={action} className="bg-content1 border border-default-100 shadow-lg rounded-md p-3 flex flex-col gap-2 ">
       <h3 className="my-4">Sign in</h3>
       <Input
         fullWidth
@@ -22,10 +29,12 @@ const SigninForm = () => {
         type="password"
         placeholder="Password"
       />
+      <SubmitButton label={"Sign in"} />
 
       <div>
         <Link href="/signup">{`Don't have an account?`}</Link>
       </div>
+      {formState?.message && (<p>{formState.message}</p>)}
     </form>
   )
 }
---
+++ utils/events.ts
@@ -0,0 +1,55 @@
+import 'server-only'
+import { db } from '@/db/db'
+import { and, asc, count, desc, eq, ne, not } from 'drizzle-orm'
+import { events, rsvps } from '@/db/schema'
+import { delay } from './delay'
+import { memoize } from 'nextjs-better-unstable-cache'
+
+
+export const getEventsForDashboard =  memoize(async (userId: string) => {
+    await delay()
+
+    const data = await db.query.events.findMany({
+        where: eq(events.createdById, userId),
+        columns: {
+          id: true,
+          name: true,
+          startOn: true,
+          status: true,
+        },
+        with: {
+          rsvps: true,
+        },
+        limit: 5,
+        orderBy: [asc(events.startOn)],
+      })
+    
+      return data ?? []
+}, 
+{
+  persist: true,
+  revalidateTags: () => ['dashboard:events'],
+}
+)
+
+export const getAllEvents = memoize(async (userId: string) => {
+  await delay()
+
+  return db.query.events.findMany({
+    where: eq(events.createdById, userId),
+    orderBy: [asc(events.startOn)],
+  })
+}, {
+  persist: true,
+  revalidateTags: () => ['events'],
+})
+
+export const getOneEvent = memoize(async (userId: string, eventId: string) => {
+  await delay();
+  return db.query.events.findFirst({
+      where: and(eq(events.id, eventId), eq(events.createdById, userId))
+  })
+}, {
+  persist: true,
+  revalidateTags: (userId, eventId) => ['events', eventId],
+})
